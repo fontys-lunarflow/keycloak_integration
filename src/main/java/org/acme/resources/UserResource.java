259928c552b3ac69d91b51a8b21169f3f3071c89
Lucas Mathews <lmath56@hotmail.com>
2025-06-01 12:00:56 +0200

    Move user endpoints to new resource

---
+++ src/main/java/org/acme/resources/UserResource.java
@@ -0,0 +1,51 @@
+package org.acme.resources;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import org.acme.services.KeycloakService;
+
+/**
+ * Resource for managing users and user information.
+ */
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @Inject
+    KeycloakService keycloakService;
+
+    @GET
+    @Path("/user/{id}")
+    @RolesAllowed({"LunarflowAdmins"})
+    public Response getUserById(@PathParam("id") String id) {
+        return Response.ok(keycloakService.getUserById(id)).build();
+    }
+
+    @GET
+    @Path("/user/username/{username}")
+    @RolesAllowed({"LunarflowAdmins"})
+    public Response getUserByUsername(@PathParam("username") String username) {
+        return Response.ok(keycloakService.getUserByUsername(username)).build();
+    }
+
+    @GET
+    @Path("/group/{groupName}")
+    @RolesAllowed({"LunarflowAdmins"})
+    public Response getUsersByGroup(@PathParam("groupName") String groupName) {
+        return Response.ok(keycloakService.getUsersByGroup(groupName)).build();
+    }
+
+    @GET
+    @Path("/all")
+    @RolesAllowed({"LunarflowAdmins"})
+    public Response getAllUsers() {
+        return Response.ok(keycloakService.getAllUsers()).build();
+    }
+}
--- src/main/java/org/acme/security/TestSecurity.java
+++ src/main/java/org/acme/security/TestSecurity.java
@@ -5,7 +5,6 @@ import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -204,34 +203,6 @@ public class TestSecurity {
         } else {
             return "None";
         }
-    }
-
-    @GET
-    @Path("/user/{id}")
-    @RolesAllowed({"LunarflowAdmins"})
-    public Response getUserById(@PathParam("id") String id) {
-        return Response.ok(keycloakService.getUserById(id)).build();
-    }
-
-    @GET
-    @Path("/user/username/{username}")
-    @RolesAllowed({"LunarflowAdmins"})
-    public Response getUserByUsername(@PathParam("username") String username) {
-        return Response.ok(keycloakService.getUserByUsername(username)).build();
-    }
-
-    @GET
-    @Path("/users/group/{groupName}")
-    @RolesAllowed({"LunarflowAdmins"})
-    public Response getUsersByGroup(@PathParam("groupName") String groupName) {
-        return Response.ok(keycloakService.getUsersByGroup(groupName)).build();
-    }
-
-    @GET
-    @Path("/users/all")
-    @RolesAllowed({"LunarflowAdmins"})
-    public Response getAllUsers() {
-        return Response.ok(keycloakService.getAllUsers()).build();
     }
 }
 
